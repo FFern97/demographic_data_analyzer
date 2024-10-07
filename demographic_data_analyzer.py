edbb880cf06b308c96281637a3a1b50265c837a6
Facundo Fernández <ffernanndez97@gmail.com>
2024-10-07 20:59:08 -0300

    Update demographic_data_analyzer.py
    Added bachelor's degree percentage

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -6,14 +6,17 @@ def calculate_demographic_data(print_data=True):
     df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = race_count = df['race'].value_counts()
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
     average_age_men = df[df['sex'] == 'Male']['age'].mean()
 
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+
+    bachelors_df = df[df['education-num'] == 13]
+    valid_rows = df['education-num'].dropna().shape[0]
+    percentage_bachelors = (bachelors_df.shape[0] / valid_rows) * 100 
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
